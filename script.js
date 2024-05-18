e69d95e1038cc477d74e65af5b373519dd46f66c
Anna Solovykh <ms.teachy.solo@gmail.com>
2024-05-17 20:00:02 -0400

    add new projects and delete unneeded data, improve animation

--- script.js
+++ script.js
@@ -70,243 +70,68 @@ function installMediaQueryWatcher(mediaQuery, layoutChangedCallback) {
 
 installMediaQueryWatcher("(min-width: 1050px)", function(matches) {
   if (matches) {
-  
-    gsap.to(".life-facts-0", {
-      text: `<span>Life facts</span>`,
-      duration: 0.5,
-      repeat: 0,
-      ease: "power1.in",
-      delay: 1})
-      
-    gsap.to(".life-facts-1", {
-        text: `Being a post-graduate in international relations, have always been passionate about <span>acquiring new knowledge.</span>`,
+
+    document.querySelector(".life-facts-0").innerHTML = `<span>Life facts</span>`;
+    document.querySelector(".life-facts-1").innerHTML = `Being a post-graduate in international relations, have always been passionate about <span>acquiring new knowledge.</span>`;
+    document.querySelector(".life-facts-2").innerHTML = `Being able to speak Russian, English, Turkish, German, and learning Spanish, <span>perceive learning languages as an exciting hobby.</span>`;
+    document.querySelector(".life-facts-3").innerHTML = `Being a former teacher of English, have always enjoyed <span>double-checking</span> in search for mistakes.`;
+    document.querySelector(".life-facts-4").innerHTML = `Being an<span> open-minded person</span>, I realised that I could enjoy that more... in coding.`;
+    document.querySelector(".life-facts-5").innerHTML = `Thus - becoming a web developer, for whom coding is a way of <span>relaxation and self-expression</span>.`;
+    document.querySelector(".why-me-0").innerHTML = `<span>Why me?</span>`;
+    document.querySelector(".why-me-1").innerHTML = `Being <span>responsible</span>, I am able to accomplish projects within time limits.`;
+    document.querySelector(".why-me-2").innerHTML = `Being <span>detail-oriented</span>, I am able to fulfill tasks up to the highest standards.`;
+    document.querySelector(".why-me-3").innerHTML = `Being <span>flexible</span>, I am able to adjust to new rules.`;
+    document.querySelector(".why-me-4").innerHTML = `Being <span>organised</span>, I am able to multitask.`;
+    document.querySelector(".why-me-5").innerHTML = `Being <span>determined</span>, I am full of desire to learn and achieve goals.`;
+
+    document.querySelector(".about-me-3").innerHTML = `Available for <span>hire</span> and open to any ideas of cooperation.`;
+
+    const elements = [
+      ".life-facts-0", ".life-facts-1", ".life-facts-2", ".life-facts-3", 
+      ".life-facts-4", ".life-facts-5", ".why-me-0", ".why-me-1", 
+      ".why-me-2", ".why-me-3", ".why-me-4", ".why-me-5", 
+      ".about-me-1", ".about-me-2", ".about-me-3"
+    ];
+
+    elements.forEach((selector, index) => {
+      gsap.from(selector, {
+        x: -100, 
+        opacity: 0,
         duration: 0.5,
-        repeat: 0,
-        ease: "power1.in",
-        delay: 1.5})
-    
-    gsap.to(".life-facts-2", {
-          text: `Being able to speak Russian, English, Turkish, German, and learning Spanish, <span> perceive learning languages as an exciting hobby.</span>`,
-          duration: 0.5,
-          repeat: 0,
-          ease: "power1.in",
-          delay: 2})
-    
-    gsap.to(".life-facts-3", {
-            text: `Being a former teacher of English, have always enjoyed <span>double-checking</span> in seach for mistakes.`,
-            duration: 0.5,
-            repeat: 0,
-            ease: "power1.in",
-            delay: 2.5})
-    
-    gsap.to(".life-facts-4", {
-              text: `Being an<span> open-minded person</span>, I realised that I could enjoy that more... in coding.`,
-              duration: 0.5,
-              repeat: 0,
-              ease: "power1.in",
-              delay: 3})
-    
-    gsap.to(".life-facts-5", {
-                text: `Thus - becoming a web developer, for whom coding is a way of <span>relaxation and self-expression</span>.`,
-                duration: 0.5,
-                repeat: 0,
-                ease: "power1.in",
-                delay: 3.5})
-    
-                gsap.to(".why-me-0", {
-                  text: `<span>Why me?</span>`,
-                  duration: 0.5,
-                  repeat: 0,
-                  ease: "power1.in",
-                  delay: 1})
-                  
-                gsap.to(".why-me-1", {
-                    text: `Being <span>responsible</span>, I am able to accomplish projects within time limits.`,
-                    duration: 0.5,
-                    repeat: 0,
-                    ease: "power1.in",
-                    delay: 1.5})
-                
-                gsap.to(".why-me-2", {
-                      text: `Being <span>detail-oriented</span>, I am able to fulfill tasks up to the highest standarts.`,
-                      duration: 0.5,
-                      repeat: 0,
-                      ease: "power1.in",
-                      delay: 2})
-                
-                gsap.to(".why-me-3", {
-                        text: `Being <span>flexible</span>, I am able to adjust to new rules.`,
-                        duration: 0.5,
-                        repeat: 0,
-                        ease: "power1.in",
-                        delay: 2.5})
-                
-                gsap.to(".why-me-4", {
-                          text: `Being <span> organised</span>, I am able to multitask.`,
-                          duration: 0.5,
-                          repeat: 0,
-                          ease: "power1.in",
-                          delay:3})
-                
-                gsap.to(".why-me-5", {
-                            text: `Being <span>determined</span>, I am full of desire to learn and achieve goals.`,
-                            duration: 0.5,
-                            repeat: 0,
-                            ease: "power1.in",
-                            delay: 3.5})
-                
-                  
-    /*<li class="why-me-0"><span>Why me?</span></li>
-              <li class="why-me-1">Being <span>responsible</span>, is able to accomplish projects within time limits.</li>
-              <li class="why-me-2"></li>>Being <span>detail-oriented</span>, is able to fulfill tasks up to the highest standarts.</li>
-              <li class="why-me-3"></li>>Being <span>flexible</span>, is able to adjust to new rules.</li>
-              <li class="why-me-4"></li>>Being <span> organised</span>, is able to multitask.</li>
-              <li class="why-me-5"></li>>Being <span>determined</span>, is full of desire to learn and achieve goals.</li>*/
-    
-              gsap.to(".about-me-1", {
-                text: `Being a <span>Russian</span>, based in <span>Turkey</span>, I am ready for cross-cultural communication and relocation.`,
-                duration: 0.5,
-                repeat: 0,
-                ease: "power1.in",
-                delay: 4})
-        
-        gsap.to(".about-me-2", {
-                  text: `Now in the process of learning <span>React</span> to further progress in web deveplopment.`,
-                  duration: 0.5,
-                  repeat: 0,
-                  ease: "power1.in",
-                  delay:4.5})
-        
-        gsap.to(".about-me-3", {
-                    text: `Available for <span>hire</span> and open to any ideas of cooperation.`,
-                    duration: 0.5,
-                    repeat: 0,
-                    ease: "power1.in",
-                    delay: 5})
-    /*
-              <p class="about-me-1">Being a <span>Russian</span>, based in <span>Turkey</span>, is ready for cross-cultural communication and further relocation.</p>
-            <p class="about-me-2">Now in the process of learning <span>React</span> to further progress in web deveplopment.</p>
-            <p class="about-me-3">Available for <span>hire</span> and open to any ideas of cooperation.</p>*/
-    
+        delay: 1 + (index * 0.1), 
+        ease: "power1.in"
+      });
+    });
+
   } else {
-    
-    gsap.to(".life-facts-0", {
-      text: `<span>Life facts</span>`,
-      duration: 0.5,
-      repeat: 0,
-      ease: "power1.in",
-      delay: 0})
-      
-    gsap.to(".life-facts-1", {
-        text: `Being a post-graduate in international relations, have always been passionate about <span>acquiring new knowledge.</span>`,
+
+    document.querySelector(".life-facts-0").innerHTML = `<span>Life facts</span>`;
+    document.querySelector(".life-facts-1").innerHTML = `Former teacher, passionate about <span>acquiring new knowledge.</span>`;
+    document.querySelector(".life-facts-2").innerHTML = `<span>Perceive learning languages as an exciting hobby.</span>`;
+    document.querySelector(".life-facts-5").innerHTML = `Coding for me is a way of <span>relaxation and self-expression</span>.`;
+    document.querySelector(".why-me-0").innerHTML = `<span>Why me?</span>`;
+    document.querySelector(".why-me-1").innerHTML = `<span>Responsible</span>: accomplish projects within time limits.`;
+    document.querySelector(".why-me-2").innerHTML = `<span>Detail-oriented</span>: fulfill tasks up to the highest standards.`;
+    document.querySelector(".why-me-3").innerHTML = `<span>Flexible</span>: able to adjust.`;
+    document.querySelector(".why-me-4").innerHTML = `<span>Organised</span>: able to multitask.`;
+    document.querySelector(".about-me-3").innerHTML = `Available for <span>hire</span> and open to any ideas of cooperation.`;
+
+    const elements = [
+      ".life-facts-0", ".life-facts-1", ".life-facts-2", ".life-facts-3", 
+      ".life-facts-4", ".life-facts-5", ".why-me-0", ".why-me-1", 
+      ".why-me-2", ".why-me-3", ".why-me-4", ".why-me-5", 
+      ".about-me-1", ".about-me-2", ".about-me-3"
+    ];
+
+    elements.forEach((selector, index) => {
+      gsap.from(selector, {
+        x: -100, 
+        opacity: 0,
         duration: 0.5,
-        repeat: 0,
-        ease: "power1.in",
-        delay: 0})
-    
-    gsap.to(".life-facts-2", {
-          text: `Being able to speak Russian, English, Turkish, German, and learning Spanish, <span>perceive learning languages as an exciting hobby.</span>`,
-          duration: 0.5,
-          repeat: 0,
-          ease: "power1.in",
-          delay: 0})
-    
-    gsap.to(".life-facts-3", {
-            text: `Being a former teacher of English, have always enjoyed <span>double-checking</span> in search for mistakes.`,
-            duration: 0.5,
-            repeat: 0,
-            ease: "power1.in",
-            delay: 0})
-    
-    gsap.to(".life-facts-4", {
-              text: `Being an<span> open-minded person</span>, I realised that I could enjoy that more... in coding.`,
-              duration: 0.5,
-              repeat: 0,
-              ease: "power1.in",
-              delay: 0})
-    
-    gsap.to(".life-facts-5", {
-                text: `Thus - becoming a web developer, for whom coding is a way of <span>relaxation and self-expression</span>.`,
-                duration: 0.5,
-                repeat: 0,
-                ease: "power1.in",
-                delay: 0})
-    
-                gsap.to(".why-me-0", {
-                  text: `<span>Why me?</span>`,
-                  duration: 0.5,
-                  repeat: 0,
-                  ease: "power1.in",
-                  delay: 0})
-                  
-                gsap.to(".why-me-1", {
-                    text: `Being <span>responsible</span>, I am able to accomplish projects within time limits.`,
-                    duration: 0.5,
-                    repeat: 0,
-                    ease: "power1.in",
-                    delay: 0})
-                
-                gsap.to(".why-me-2", {
-                      text: `Being <span>detail-oriented</span>, I am able to fulfill tasks up to the highest standarts.`,
-                      duration: 0.5,
-                      repeat: 0,
-                      ease: "power1.in",
-                      delay: 0})
-                
-                gsap.to(".why-me-3", {
-                        text: `Being <span>flexible</span>, I am able to adjust to new rules.`,
-                        duration: 0.5,
-                        repeat: 0,
-                        ease: "power1.in",
-                        delay: 0})
-                
-                gsap.to(".why-me-4", {
-                          text: `Being <span> organised</span>, I am able to multitask.`,
-                          duration: 0.5,
-                          repeat: 0,
-                          ease: "power1.in",
-                          delay: 0})
-                
-                gsap.to(".why-me-5", {
-                            text: `Being <span>determined</span>, I am full of desire to learn and achieve goals.`,
-                            duration: 0.5,
-                            repeat: 0,
-                            ease: "power1.in",
-                            delay: 0})
-                
-                  
-    /*<li class="why-me-0"><span>Why me?</span></li>
-              <li class="why-me-1">Being <span>responsible</span>, is able to accomplish projects within time limits.</li>
-              <li class="why-me-2"></li>>Being <span>detail-oriented</span>, is able to fulfill tasks up to the highest standarts.</li>
-              <li class="why-me-3"></li>>Being <span>flexible</span>, is able to adjust to new rules.</li>
-              <li class="why-me-4"></li>>Being <span> organised</span>, is able to multitask.</li>
-              <li class="why-me-5"></li>>Being <span>determined</span>, is full of desire to learn and achieve goals.</li>*/
-    
-              gsap.to(".about-me-1", {
-                text: `Being a <span>Russian</span>, based in <span>Turkey</span>, I am ready for cross-cultural communication and relocation.`,
-                duration: 0.5,
-                repeat: 0,
-                ease: "power1.in",
-                delay: 0})
-        
-        gsap.to(".about-me-2", {
-                  text: `Now in the process of learning <span>React</span> to further progress in web deveplopment.`,
-                  duration: 0.5,
-                  repeat: 0,
-                  ease: "power1.in",
-                  delay: 0})
-        
-        gsap.to(".about-me-3", {
-                    text: `Available for <span>hire</span> and open to any ideas of cooperation.`,
-                    duration: 0.5,
-                    repeat: 0,
-                    ease: "power1.in",
-                    delay: 0})
-    /*
-              <p class="about-me-1">Being a <span>Russian</span>, based in <span>Turkey</span>, is ready for cross-cultural communication and further relocation.</p>
-            <p class="about-me-2">Now in the process of learning <span>React</span> to further progress in web deveplopment.</p>
-            <p class="about-me-3">Available for <span>hire</span> and open to any ideas of cooperation.</p>*/
-    
+        delay: index * 0.1, 
+        ease: "power1.in"
+      });
+    });
   }
 });
 
